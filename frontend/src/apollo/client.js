5d76a1d371c1b0e25778e8daaa11de3ea373f595
João Anes <joaoanes@lifeonmars.pt>
2018-02-18 04:50:48 +0000

    Small eslint fix

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -1,6 +1,6 @@
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 import fp from 'lodash/fp'
 import { memoize } from 'lodash'
@@ -8,7 +8,7 @@ import { memoize } from 'lodash'
 export default new ApolloClient({
   link: new HttpLink({ uri: 'http://localhost:4000/api/graphiql' }),
   cache: new InMemoryCache()
-});
+})
 
 const addProps = f => initialProps => ({
   ...initialProps,
@@ -37,9 +37,7 @@ export const paginated = (connectionPath, dataPath = 'data') => {
     )
 
   return fp.update(dataPath, data => {
-    console.log(data.networkStatus)
     if (data.networkStatus === 1 || data.networkStatus === 4) {
-      console.log("clearing cache")
       generateNextPage.cache.clear()
     }
     return fp.update(
