5d704c205a63e3d9698470e725593fcb149d3fa2
João Anes <joaoanes@frctls.com>
2019-05-27 01:50:12 +0100

    Add invalidation to deploy process

--- frontend-refresh/deploy.sh
+++ frontend-refresh/deploy.sh
@@ -2,3 +2,4 @@
 
 yarn build
 REACT_APP_TAG=$(git describe --tags) REACT_APP_COMMIT=$(git rev-parse HEAD) AWS_PROFILE=joaoanes aws s3 sync ./build s3://shitpost-network-cf/ --delete
+AWS_PROFILE=joaoanes aws cloudfront create-invalidation --distribution-id E29A9QENZYC2D5 --paths /index.html
