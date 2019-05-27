8ee0adeee9e9344be6f7c377fa44ba1ad61fa836
João Anes <joaoanes@frctls.com>
2019-05-27 02:37:44 +0100

    Remove service worker
    Add / to invalidations path

--- frontend-refresh/deploy.sh
+++ frontend-refresh/deploy.sh
@@ -2,4 +2,4 @@
 
 yarn build
 AWS_PROFILE=joaoanes aws s3 sync ./build s3://shitpost-network-cf/ --delete
-AWS_PROFILE=joaoanes aws cloudfront create-invalidation --distribution-id E29A9QENZYC2D5 --paths /index.html
+AWS_PROFILE=joaoanes aws cloudfront create-invalidation --distribution-id E29A9QENZYC2D5 --paths / /index.html
