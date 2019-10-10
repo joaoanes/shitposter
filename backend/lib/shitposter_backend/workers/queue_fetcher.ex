f6ddf5abc29d5b0115844f2fdf82b4efc157736b
João Anes <joao.anes@gmail.com>
2019-10-10 14:14:49 +0100

    Refactor queue fetcher

--- backend/lib/shitposter_backend/workers/queue_fetcher.ex
+++ backend/lib/shitposter_backend/workers/queue_fetcher.ex
@@ -14,19 +14,24 @@ defmodule ShitposterBackend.Workers.QueueFetcher do
     {:ok, %{body: %{messages: messages}}} =
       ExAws.SQS.receive_message("scraper-upload-queue.fifo", [max_number_of_messages: 10]) |> ExAws.request(region: "eu-central-1")
 
-    new_shitposts = Enum.map(messages, fn %{body: bod} ->
+    new_shitposts = Enum.map(messages, fn message ->
+      %{body: bod} = message
       {:ok, [url, meta]} = Poison.decode(bod)
 
       %{"postedAt" => posted_at, "sourceLink" => source_link, "hashtags" => hashtags} = meta
 
-      ShitposterBackend.Shitpost.create(url, nil, user, nil, [], posted_at, source_link, hashtags)
+      case ShitposterBackend.Shitpost.create(url, nil, user, nil, [], posted_at, source_link, hashtags) do
+        {:ok, shitpost} -> (
+          ExAws.SQS.delete_message("scraper-upload-queue.fifo", message.receipt_handle)
+          |> ExAws.request(region: "eu-central-1")
+
+          shitpost.id
+        )
+        _ -> nil
+      end
     end
     )
 
-    ExAws.SQS.delete_message_batch("scraper-upload-queue.fifo", Enum.map(messages, fn %{message_id: mid, receipt_handle: rec} ->
-      %{id: mid, receipt_handle: rec}
-    end)) |> ExAws.request(region: "eu-central-1")
-
-    Enum.map(new_shitposts, fn {:ok, shitpost} -> shitpost.id end)
+    new_shitposts
   end
 end
